50e42d2f78df9c20cdfcc16f14874517be27efd8
Dmitry_Kruglov <Dmitrykruglov1993@gmail.com>
2020-10-21 16:30:38 +0300

    Задача 1_1_3

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,99 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import javax.naming.Name;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl extends Util implements UserService {
+
+    Connection connection = getConnection();
+
     public void createUsersTable() {
+
+        String sql = "CREATE TABLE Users(id int NOT NULL AUTO_INCREMENT," +
+                "Name varchar (20) NOT NULL," +
+                "LastName varchar (20)," +
+                "Age INT NOT NULL," +
+                "PRIMARY KEY(id))";
+        try(Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
     public void dropUsersTable() {
+        String sql = "DROP TABLE Users";
 
+        try(Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
 
-    }
+        String sql = "INSERT INTO Users (name, lastName, age)" +
+                " VALUE ('"+ name +"','"+ lastName +"', '"+ age +"')";
+
+        try(Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+            System.out.println("User с именем ' "+ name +"' добавлен в базу данных");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+   }
+
 
     public void removeUserById(long id) {
+        User user = new User();
+        String sql = "DELETE FROM Users WHERE id = '" + id + "'";
 
+        try(Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+
+        List<User> UserList = new ArrayList<>();
+
+        String sql = "SELECT * FROM Users";
+
+        try(Statement statement = connection.createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastname"));
+                user.setAge(resultSet.getByte("age"));
+                UserList.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return UserList;
     }
 
     public void cleanUsersTable() {
+        String sql = "DELETE FROM Users";
 
+        try(Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final String URL="jdbc:mysql://localhost:3306/mysql?serverTimezone=Europe/Moscow&useSSL=false";
+    private final String USER_NAME="root";
+    private final String PASSWORD="1234";
+
+    public Connection getConnection(){
+        Connection connectoin = null;
+        try {
+           connectoin = DriverManager.getConnection(URL,USER_NAME,PASSWORD);
+            System.out.println("Connection good");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection Error");
+        }
+        return connectoin;
+    }
 }
